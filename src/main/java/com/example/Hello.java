6f16c088c0e5917fac0b85855f6f89665f1b907c
Ankitha Rao <105905160+Ankithasrao@users.noreply.github.com>
2025-11-24 19:41:51 +0530

    Update Hello.java

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Java 8 Maven App! 🚀");
+        System.out.println("Hello from Java 🚀");
     }
 }
